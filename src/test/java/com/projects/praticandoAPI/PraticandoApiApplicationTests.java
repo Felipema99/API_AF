5f1fa6464593924c4d8f0853309cbe08377370a9
Felipema99 <72240062+Felipema99@users.noreply.github.com>
2023-06-07 18:13:11 -0300

    Update PraticandoApiApplicationTests.java

--- src/test/java/com/projects/praticandoAPI/PraticandoApiApplicationTests.java
+++ src/test/java/com/projects/praticandoAPI/PraticandoApiApplicationTests.java
@@ -9,10 +9,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.projects.praticandoAPI.controller.UsuarioController;
+import com.projects.praticandoAPI.controller.WelloWorld;
 
 
-//
+
 @RunWith(SpringRunner.class)
 //@SpringBootTest 
 @WebMvcTest(controllers = HelloWorld.class) 
@@ -30,7 +30,7 @@ public class PraticandoApiApplicationTests {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Felipe Martins de Almeida!"));
     }
-//
+
 
 }
 
